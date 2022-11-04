6547620c4d8aacfbce2d9407c3c9daa43542a999
willi <willi@LAPTOP-8H8QL02R>
2022-11-04 14:36:41 -0500

    Agregar archivos de proyecto.

---
+++ ViajesMetroPlus_Pruebas/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ViajesMetroPlus_Pruebas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ViajesMetroPlus_Pruebas")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f10adcd2-22d6-493d-9876-e9c0957c854d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ViajesMetroPlus_Pruebas/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
+
+public class testclass
+{
+    [Fact]
+    public void TesteoObtieneRutaMenorPasajeros()
+    {
+        string devuelve;
+        string[] devuelve1 = { "NombrePrueba1", "NombrePrueba2", "NombrePrueba3", "NombrePrueba4", "NombrePrueba5" };
+        int devuelve2 = 0;
+        int[] devuelve3 = { 1, 2, 3, 4, 5 };
+        Assert.True(ex03_ViajesMetroplus.Program.ObtieneRutaMenorPasajeros(devuelve3,
+                devuelve1,
+                out devuelve,
+                out devuelve2));
+    }
+
+    [Fact]
+    public void TesteoTotalizaPasajerosRuta()
+    {
+        string[] devuelve1 = { "NombrePrueba1", "NombrePrueba2", "NombrePrueba3", "NombrePrueba4", "NombrePrueba5" };
+
+        Random aleatorio = new Random();
+        ex03_ViajesMetroplus.Viaje[] losViajes = new ex03_ViajesMetroplus.Viaje[100];
+
+        for (int i = 0; i < losViajes.Length; i++)
+        {
+            losViajes[i] = new ex03_ViajesMetroplus.Viaje();
+            losViajes[i].Ruta = devuelve1[aleatorio.Next(devuelve1.Length)];
+            losViajes[i].CantidadPasajeros = aleatorio.Next(5, 41);
+        }
+        Assert.NotEmpty(ex03_ViajesMetroplus.Program.TotalizaPasajerosRuta(losViajes, devuelve1));
+    }
+
+}
+
